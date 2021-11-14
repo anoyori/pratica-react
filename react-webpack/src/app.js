9ad6d228b81876230b5be263c0f3a832d5959ff2
anoyori <anoyori@gmail.com>
2021-11-14 00:10:01 -0300

    Lifecycle dos componentes

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,19 +1,37 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Timer from './timer'
 
 class App extends Component {
   constructor () {
+    console.log('constructor')
     super()
     this.state = {
-      text: 'Amanda'
+      showTimer: true
     }
   }
 
+  UNSAFE_componentWillMount () {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount () {
+    console.log('componetDidMount')
+  }
+
   render () {
+    console.log('render')
     return (
-      <div className='container' onClick={() => this.setState({ text: 'outro texto' })}>
-        {this.state.text}
+      <div>
+        {this.state.showTimer && <Timer />}
+
+        <button onClick={() => {
+          this.setState({ showTimer: !this.state.showTimer })
+        }}
+        >
+          Show / Hide Timer
+        </button>
       </div>
     )
   }
